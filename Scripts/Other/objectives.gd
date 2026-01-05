5e75575b9429fc575ce6ac55541ac0970ddd6311
KristianNikolov07 <kris.nikolov07@gmail.com>
2026-01-05 15:31:53 +0200

    Added keybind regexes to the objective descriptions

--- Scripts/Other/objectives.gd
+++ Scripts/Other/objectives.gd
@@ -24,7 +24,17 @@ func set_objective(objective : String) -> void:
 
 
 func update_ui() -> void:
-	$Description.text = objectives[current_objective]["description"]
+	var description : String = objectives[current_objective]["description"]
+	var regex = RegEx.new()
+	regex.compile(r"(?<=CONTROL\()[^)]+(?=\))")
+	for action in regex.search_all(description):
+		print(action.get_string())
+		if InputMap.has_action(action.get_string()) and InputMap.action_get_events(action.get_string()).size() > 0:
+			var event = InputMap.action_get_events(action.get_string())[0]
+			var keybind = event.as_text().replace("(Physical)", "").lstrip(" ").rstrip(" ")
+			description = description.replace("CONTROL(" + action.get_string() + ")", keybind)
+	
+	$Description.text = description
 
 
 func complete_objective(objective : String) -> void:
