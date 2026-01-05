996485bb81ac23dbb05f3871d75d155fc3f67e06
KristianNikolov07 <kris.nikolov07@gmail.com>
2026-01-05 21:35:24 +0200

    Added more translations

---
+++ Scripts/Classes/Biome.gd
@@ -0,0 +1,8 @@
+class_name Biome
+extends Area2D
+
+@export var objects : Array[SpawnChance]
+
+@export_group("Entities")
+@export var passive_entities : Array[SpawnChance]
+@export var enemies : Array[SpawnChance]
---
+++ Scripts/Classes/SpawnChance.gd
@@ -0,0 +1,5 @@
+class_name SpawnChance
+extends Resource
+
+@export var scene : PackedScene
+@export_range(0, 100) var change : int
