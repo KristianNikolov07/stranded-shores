ada9171cef157bb0848fcd072ae5c5be04cdf850
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2026-01-10 17:31:34 +0200

    Cleanup

--- Scripts/Objects/crafting_table.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-
-func interact(player : Player) -> void:
-	player.crafting.open_menu(true)
--- Scripts/Objects/repair_station.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-func interact(player : Player) -> void:
-	$CanvasLayer/RepairMenu.show()
-	$CanvasLayer/RepairMenu.update_ui()
-	player.repair_menu = $CanvasLayer/RepairMenu
-	player.can_move = false
