916641c5c86f7dbba3c90c873616b51f8e57e36f
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2026-01-13 21:03:11 +0200

    Fixed the pickaxe and added the breaking textures for the big rocks

--- Scripts/Tools/pickaxe.gd
+++ Scripts/Tools/pickaxe.gd
@@ -10,13 +10,13 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_released("Attack"):
 		$CollisionShape2D.disabled = true
 		is_used = false
-		print("stopped using")
 
 
 func use() -> void:
 	look_at(get_global_mouse_position())
-	$CollisionShape2D.disabled = false
-	is_used = true
+	if $Cooldown.is_stopped():
+		$CollisionShape2D.disabled = false
+		is_used = true
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -25,11 +25,9 @@ func _on_body_entered(body: Node2D) -> void:
 		hit.emit()
 		$CollisionShape2D.set_deferred("disabled", true)
 		$Cooldown.start()
-		print("hit")
 
 
 func _on_cooldown_timeout() -> void:
-	$CollisionShape2D.disabled = false
-	print("timer")
 	if is_used:
+		$CollisionShape2D.disabled = false
 		$Cooldown.start()
