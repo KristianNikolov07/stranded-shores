13bd4a031390987a5e2ad4fd55c8c801bd338e1a
KristianNikolov07 <kris.nikolov07@gmail.com>
2026-01-19 15:36:06 +0200

    Arrows shot by the player now deal more damage

--- Scenes/Tools/bow.gd
+++ Scenes/Tools/bow.gd
@@ -4,6 +4,8 @@ signal hit
 
 const ARROW_SCENE = preload("res://Scenes/Objects/arrow.tscn")
 
+@export var damage = 20
+
 func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
 
@@ -13,6 +15,7 @@ func use() -> void:
 	if player.inventory.has_item("Arrow"):
 		var arrow = ARROW_SCENE.instantiate()
 		arrow.global_position = $ShootPoint.global_position
+		arrow.damage = damage
 		arrow.projectile_owner = player
 		arrow.target = get_global_mouse_position()
 		get_tree().current_scene.add_child(arrow)
