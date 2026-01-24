14f2f0050b3d89638dcccc0a518c9cdd33fe9d45
KristianNikolov07 <kris.nikolov07@gmail.com>
2026-01-24 20:00:52 +0200

    Fixed the revert

--- Scripts/Classes/Items/Consumables/Food.gd
+++
@@ -1,9 +0,0 @@
-class_name Food
-extends Consumable
-
-@export var cooked_version : Food
-
-func cook():
-	if cooked_version != null:
-		if Global.get_player().inventory.add_item(cooked_version.duplicate()):
-			Global.get_player().inventory.remove_item(item_name)
